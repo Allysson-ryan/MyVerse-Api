6fa1655752105bcfeb52dec84e7fc91288bd5855
Allysson Ryan <allyssonr002@gmail.com>
2025-06-24 09:09:09 -0300

    updated

--- Src/app.js
+++ Src/app.js
@@ -15,7 +15,13 @@ const allowedOrigins = process.env.CORS_ORIGIN?.split(",") || [];
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
