c004f0e29c5a790c3c3d66be95aa33aca94bdd02
Allysson Ryan <allyssonr002@gmail.com>
2025-06-24 09:24:38 -0300

    updated

--- Src/middleware/authMiddleware.js
+++ Src/middleware/authMiddleware.js
@@ -2,16 +2,17 @@ const jwt = require("jsonwebtoken");
 const User = require("../Model/UserModel");
 
 module.exports = async function authMiddleware(req, res, next) {
-  const token = req.headers.authorization?.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).json({ error: "Token não fornecido." });
-  }
-
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.id);
+    const authHeader = req.headers.authorization;
 
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ error: "Token não fornecido." });
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id);
     if (!user) {
       return res.status(401).json({ error: "Usuário não autorizado." });
     }
